a94f723f35b351e2ccf4bd7340806fb307e3ce07
Pabl0Parra <pabsblue16@gmail.com>
2022-06-25 10:12:10 +0200

    finished ex_6 - needs styling

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,6 +1,10 @@
 import Quantity from "./Quantities";
+import Popup from "./Popup";
+import { useState } from "react";
 
 function Panel(props) {
+  const [buttonPopup, setButtonPopup] = useState(false);
+
   return (
     <div className="panel">
       <div>
@@ -11,6 +15,10 @@ function Panel(props) {
           addProduct={props.addProduct}
         />
       </div>
+      <button onClick={() => setButtonPopup(true)}>Popup</button>
+      <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+        <h3>Please, enter the number of webpages you will need</h3>
+      </Popup>
       <div>
         Languages:
         <Quantity
@@ -19,6 +27,10 @@ function Panel(props) {
           addProduct={props.addProduct}
         />
       </div>
+      <button onClick={() => setButtonPopup(true)}>Popup</button>
+      <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+        <h3>Please, enter the number of languages your site will need</h3>
+      </Popup>
     </div>
   );
 }
