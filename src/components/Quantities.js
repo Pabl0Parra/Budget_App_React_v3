38d64d63aeb17ddad588f5e1eb6abdd14ae98cdb
Pabl0Parra <98957023+Pabl0Parra@users.noreply.github.com>
2022-06-27 13:16:01 +0200

    extra styles

--- src/components/Quantities.js
+++ src/components/Quantities.js
@@ -1,16 +1,17 @@
 export default function Quantity(props) {
   function handleOnChange(variation) {
+    const quantity = parseInt(props.budgetFormData[props.id]) + variation;
     props.addProduct({
       type: "text",
       name: props.id,
-      value: parseInt(props.budgetFormData[props.id]) + variation,
+      value: isNaN(quantity) ? 1 : quantity,
     });
   }
   return (
     <div className="quantity">
       <button onClick={() => handleOnChange(1)}>+</button>
       <input
-        type="text"
+        type="number"
         onChange={(event) => props.addProduct(event.target)}
         id={props.id}
         name={props.id}
