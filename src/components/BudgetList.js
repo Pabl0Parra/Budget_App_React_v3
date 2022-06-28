f44c08feffcd2c35064ff5682bd00fe2039e6209
Pabl0Parra <pabsblue16@gmail.com>
2022-06-28 21:44:51 +0200

    finished ex_7

--- src/components/BudgetList.js
+++ src/components/BudgetList.js
@@ -1,31 +1,47 @@
-export const BudgetList = (props) => {
-  const List = props.budgetList.map((budget, i) => (
-    <div className="budget-list" key={i}>
-      <br />
-      <p>Budget creation time: {budget.date}</p>
-      <p>
-        Budget name:
-        {budget.budgetName}
-      </p>
-      <p>
-        User name:
-        {budget.userName}
-      </p>
-      <p>
-        Chosen services:
-        {budget.webSite &&
-          `Website with: ${budget.pages} page(s) & ${budget.languages} language(s). `}
-        {budget.seo && "SEO consultancy."}
-        {budget.google && "Google Ads Campaign"}
-      </p>
+import "../styles/BudgetCard.css";
+import NumberFormat from "react-number-format";
 
-      <p>
-        Total price:
-        {budget.price}
-      </p>
-      <br />
+export const BudgetList = (props) => {
+  const Card = props.budgetList.map((budget, i) => (
+    <div className="budget-list card-wrapper" key={i}>
+      <div className="card-title">
+        <span>Budget title: {budget.budgetName}</span>
+        <div className="card-name">
+          <span>User Name: {budget.userName}</span>
+        </div>
+        <hr></hr>
+        <div className="card-description">
+          <span>
+            ·{" "}
+            {budget.webSite &&
+              `Website with: ${budget.pages} page(s) & ${budget.languages} language(s). `}
+          </span>
+          <br></br>
+          <span>· {budget.seo && "SEO consultancy."}</span>
+          <br></br>
+          <span>· {budget.google && "Google Ads Campaign"}</span>
+        </div>
+        <hr></hr>
+        <div className="card-price">
+          <span>
+            Total price =
+            <NumberFormat
+              id="NumberFormat"
+              value={budget.price}
+              displayType={"text"}
+              thousandSeparator={true}
+              prefix={"$"}
+              renderText={(value, props) => <div {...props}>{value}</div>}
+            />
+          </span>
+          <hr></hr>
+        </div>
+        <div className="card-date">
+          <span>{budget.date}</span>
+        </div>
+      </div>
     </div>
   ));
 
-  return List;
+  return Card;
 };
